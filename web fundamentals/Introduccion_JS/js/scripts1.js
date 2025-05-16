86e70b9207cfe26c234858de35eb44732d9a9677
Miguel Angel <miguel43155957@gmail.com>
2025-05-15 23:16:16 -0500

    js-advance1

---
+++ web fundamentals/Introduccion_JS/js/scripts1.js
@@ -0,0 +1,22 @@
+// Variables
+
+var product = "Computer Gamer"; // start variable and assign value
+
+var store; // start variable with no value
+
+product = true; // reassigning the value of the variable
+
+store = false;
+
+var product2 = "ipad",
+    available = true,
+    category = "technology"; // declaring multiple variables
+
+
+// styles for variables
+var name_product = "ifhone"; // underscore
+var nameProduct = "samsung"; //camelcase 
+var NameProduct = "ifhone15";  // pascal case
+var nombreproducto = "sansung x"; 
+
+console.log(product)
