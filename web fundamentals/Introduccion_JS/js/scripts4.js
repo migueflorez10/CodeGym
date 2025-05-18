c33fc7b602e977c791e05834945b1c6447343cf9
Miguel Angel <miguel43155957@gmail.com>
2025-05-17 23:19:39 -0500

    js-advance2.0

--- web fundamentals/Introduccion_JS/js/scripts4.js
+++ web fundamentals/Introduccion_JS/js/scripts4.js
@@ -7,3 +7,24 @@ const product3 = new String("Monitor 32 pg");
 console.log(typeof product)
 console.log(typeof product2)
 console.log(typeof product3)
+
+console.log(miVar); 
+var miVar = 5;
+console.log(miVar); 
+
+function ejemplo() {
+    var x = 1;
+    if (true) {
+        var x = 2; 
+        console.log(x); 
+    }
+    console.log(x); 
+}
+ejemplo();
+
+
+const PI = 3.1416;
+
+const usuario = { nombre: "Ana" };
+usuario.nombre = "Luis"; 
+console.log(usuario.nombre); // "Luis"
