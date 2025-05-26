babe5f59a08997d9c16c828caee8fedfa1c6e9e3
Miguel Angel <miguel43155957@gmail.com>
2025-05-25 21:57:00 -0500

    js_DOM_Advance1

---
+++ web fundamentals/JavaScript_DOM_Events_start/js/scripts.js
@@ -0,0 +1,14 @@
+// querySelector
+
+const heading = document.querySelector('.header__texto h2'); // 0 or 1 elements
+heading.textContent = 'new course'
+
+
+
+// querySelectorAll 
+
+
+
+
+
+// GetElementById
