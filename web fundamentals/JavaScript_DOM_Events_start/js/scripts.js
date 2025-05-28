29446794c781a2724696890ca3057353f0eb0995
Miguel Angel <miguel43155957@gmail.com>
2025-05-27 23:25:17 -0500

    js_DOM_Finished

--- web fundamentals/JavaScript_DOM_Events_start/js/scripts.js
+++ web fundamentals/JavaScript_DOM_Events_start/js/scripts.js
@@ -37,32 +37,105 @@ console.log(nuevoEnlace);
 
 // events
 
-console.log(1);
+// console.log(1);
 
-window.addEventListener('load', ()=>{   // load waits until the js and html dependent files are ready.
-    console.log(2);
-})
+// window.addEventListener('load', ()=>{   // load waits until the js and html dependent files are ready.
+//     console.log(2);
+// })
 
-window.onload = () => {
-    console.log(10);
-}
+// window.onload = () => {
+//     console.log(10);
+// }
 
-document.addEventListener('DOMContentLoaded', () => { // only waits for HTML, but doesn't wait for CSS or Images
-    console.log(3);
-})
+// document.addEventListener('DOMContentLoaded', () => { // only waits for HTML, but doesn't wait for CSS or Images
+//     console.log(3);
+// })
 
-console.log(5);
+// console.log(5);
 
-window.onscroll = (event) => {
-    console.log(event)
-}
+// window.onscroll = (event) => {
+//     console.log(event)
+// }
 
 // select elemnets and asing an event to them
 
-const btnEnviar = document.querySelector('.boton--primario');
-btnEnviar.addEventListener('click', (event) => {
-    console.log(event);
-    event.preventDefault();
-    // validate form
-    console.log('send....')
+// const btnEnviar = document.querySelector('.boton--primario');
+// btnEnviar.addEventListener('click', (event) => {
+//     console.log(event);
+//     event.preventDefault();
+//     // validate form
+//     console.log('send....')
+// })
+
+
+// inputs events and textarea
+
+const datos = {
+    nombre: '',
+    email: '',
+    mensaje: ''
+}
+
+const nombreInput = document.querySelector('#nombre');
+const emailInput = document.querySelector('#email');
+const mensajeInput = document.querySelector('#mensaje');
+const formulario = document.querySelector('.formulario');
+
+nombreInput.addEventListener('input', leerTexto);
+
+emailInput.addEventListener('input', leerTexto);
+
+mensajeInput.addEventListener('input', leerTexto);
+
+formulario.addEventListener('submit', (evento)=>{
+    evento.preventDefault();
+    // valid form
+
+    const {nombre, email, mensaje} = datos;
+
+    if(nombre === '' || email === '' || mensaje === ''){
+        mostrarError('Todos los campos son obligatorios')
+        return; // cust de code execution
+    }
+
+    mensajeExito('Formulario enviado con exito')
+
+    // send form
+
+    console.log('Send Form')
 })
+
+function leerTexto(e){
+    // console.log(e.target.value);
+    datos[e.target.id] = e.target.value; 
+    // console.log(datos);
+}
+
+// display error on screen
+function mostrarError(mensaje){
+    const error = document.createElement('p');
+    error.textContent = mensaje; 
+    error.classList.add('error');
+    formulario.appendChild(error);
+
+
+    // message appears after three seconds
+    setTimeout(() => {
+        error.remove()
+    }, 5000);
+};
+
+// message - correct form
+function mensajeExito(mensaje){
+    const correcto = document.createElement('p');
+    correcto.textContent = mensaje; 
+    correcto.classList.add('correcto');
+    formulario.appendChild(correcto);
+
+
+    // message appears after three seconds
+    setTimeout(() => {
+        correcto.remove()
+    }, 5000);
+};
+
