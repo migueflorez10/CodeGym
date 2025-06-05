a53b94bd8ff6580b781bcd13b9907c2f62b9a60f
Miguel Angel <miguel43155957@gmail.com>
2025-06-04 21:18:36 -0500

    musicFestivalProject- Advance2

--- web fundamentals/Music Festival Materials/gulpfile.js
+++ web fundamentals/Music Festival Materials/gulpfile.js
@@ -1,4 +1,17 @@
-export function hello(done){
-    console.log('hello from Gulpfile.js');
-    done();
+import {src, dest, watch} from 'gulp'
+import * as dartSass from 'sass'
+import gulpSass from 'gulp-sass'
+
+const sass = gulpSass(dartSass)
+
+export function css(done){
+    src('src/scss/app.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(dest('build/css'))
+    done()
 }
+
+export function dev(){
+    watch('src/scss/**/*.scss', css)
+}
+
