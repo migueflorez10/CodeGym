09119bd44c302e7f69837b7c48b3f4184e65c356
Miguel Angel <miguel43155957@gmail.com>
2025-06-16 22:38:53 -0500

    musicFestivalProject- finished

--- web fundamentals/Music Festival Materials/src/js/app.js
+++ web fundamentals/Music Festival Materials/src/js/app.js
@@ -1,6 +1,22 @@
 document.addEventListener('DOMContentLoaded', ()=>{
+    navegacionFija();
     crearGaleria();
+    resaltarEnlace();
+    scrollNav();
 })
+
+function navegacionFija(){
+    const header = document.querySelector('.header');
+    const sobreFestival = document.querySelector('.sobre-festival');
+
+    window.addEventListener('scroll', ()=>{
+        if(sobreFestival.getBoundingClientRect().bottom < 1){
+            header.classList.add('fixed');
+        } else {
+            header.classList.remove('fixed');
+        }
+    })
+}
 
 function crearGaleria(){
     const cantidad_imagenes = 16;
@@ -21,13 +37,72 @@ function crearGaleria(){
 }
 
 function mostrarImagen(i){
+    const imagen = document.createElement('IMG');
+    imagen.src = `src/img/gallery/full/${i}.jpg`;
+    imagen.alt = 'Imagen Galeria';
+    
     // generar Modal
-
     const modal = document.createElement('DIV');
     modal.classList.add('modal');
-    modal.onclick = cerrarModal
+    modal.onclick = cerrarModal;
+
+    // boton de cerrar modal
+    const cerrarModalBtn = document.createElement('BUTTON');
+    cerrarModalBtn.textContent = 'X';
+    cerrarModalBtn.classList.add('btn-cerrar');
+    cerrarModalBtn.onclick = cerrarModal;
+
+    modal.appendChild(imagen);
+    modal.appendChild(cerrarModalBtn);
 
     // agregar al html
     const body = document.querySelector('body');
+    body.classList.add('overflow-hidden');
     body.appendChild(modal);
+}
+
+function cerrarModal(){
+    const modal = document.querySelector('.modal');
+    modal.classList.add('fadeOut');
+    setTimeout(() => {
+        modal?.remove();
+        const body = document.querySelector('body');
+        body.classList.remove('overflow-hidden')
+    }, 500);
+}
+
+function resaltarEnlace() {
+    window.addEventListener('scroll', () => {
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('.navegacion-principal a');
+    let actual = '';
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+
+    if (window.scrollY >= (sectionTop - sectionHeight / 3)) {
+        actual = section.id;
+        }
+    });
+
+    navLinks.forEach(link => {
+            link.classList.remove('active'); 
+    if (link.getAttribute('href') === '#' + actual) {
+            link.classList.add('active');  
+            }
+        });
+    });
+}
+
+function scrollNav(){
+    const navLinks = document.querySelectorAll('.navegacion-principal a');
+    navLinks.forEach(link =>{
+        link.addEventListener('click', (e) => {
+            e.preventDefault()
+            const sectionScroll = e.target.getAttribute('href');
+            const section = document.querySelector(sectionScroll);
+            section.scrollIntoView({behavior: 'smooth'})
+        })
+    })
 }
