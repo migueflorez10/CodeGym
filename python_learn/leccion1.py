d01898736104cb385dd95a6173d5d2604ce22456
Miguel Angel <mamartinef@eafit.edu.co>
2025-11-21 22:54:17 -0500

    feat: add examples of variables and basic operations

---
+++ python_learn/leccion1.py
@@ -0,0 +1,32 @@
+# Numeros
+a = 10
+b = 3   
+
+# Suma
+suma = a + b
+print("Suma:", suma)
+# Resta
+resta = a - b
+print("Resta:", resta)
+#Multiplicacion
+multi = a * b
+print("Multiplicacion:", multi)
+# Division (entero)
+division = a//b
+print("Division (entero):", division)
+#Division (flotante)
+division2 = a/b
+print("Division (flotante):", division2)
+# Modulo 
+modulo = a % b
+print("Modulo:", modulo)
+# Potencia
+potencia = a ** b
+print("Potencia:", potencia)
+
+
+# Ejemplo 
+nota_1 = 9
+nota_2 = 7
+nota_media = (nota_1 + nota_2) / 2
+print("La nota media es:", nota_media)
