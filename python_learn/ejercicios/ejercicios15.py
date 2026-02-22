eaa88a58e1f0ecef02e5eb7f2a87b4e74554fff2
Miguel Angel <mamartinef@eafit.edu.co>
2026-02-21 22:55:30 -0500

    Section: methods - exercises

--- python_learn/ejercicios/ejercicios15.py
+++ python_learn/ejercicios/ejercicios15.py
@@ -2,7 +2,14 @@
 
 # Completa el ejercicio aquí
 texto = "un día que el viento soplaba con fuerza#mira como se mueve aquella banderola -dijo un monje#lo que se mueve es el viento -respondió otro monje#ni las banderolas ni el viento, lo que se mueve son vuestras mentes -dijo el maestro"
-resultado = '\n'.join(texto.split('-'))
-resultado = '\n'.join(texto.split('#'))
 
-print(resultado)
+lineas = texto.split('#')
+for i, linea in enumerate(lineas):
+    lineas[i] = linea.capitalize()
+    if i == 0:
+        lineas[i] = lineas[i] + '...'
+    else: 
+        lineas[i] = '- ' + lineas[i] + '.'
+
+for linea in lineas:
+    print(linea)
